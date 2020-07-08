120b5d7cdf1d9d4d6f78b9011083e4668ce15bdb
Nico Rusam <nico.rusam@gmail.com>
2020-07-08 19:29:25 +0200

    Finished Xylophone App

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,39 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
+  Expanded xyloButton({Color color, String soundFile}) => Expanded(
+        child: FlatButton(
+          color: color,
+          onPressed: () => player.play(soundFile),
+          child: null,
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              xyloButton(color: Colors.red, soundFile: 'note1.wav'),
+              xyloButton(color: Colors.orange, soundFile: 'note2.wav'),
+              xyloButton(color: Colors.yellow, soundFile: 'note3.wav'),
+              xyloButton(color: Colors.green, soundFile: 'note4.wav'),
+              xyloButton(color: Colors.teal, soundFile: 'note5.wav'),
+              xyloButton(color: Colors.blue, soundFile: 'note6.wav'),
+              xyloButton(color: Colors.purple, soundFile: 'note7.wav'),
+              xyloButton(color: Colors.brown, soundFile: 'fart.wav'),
+            ],
+          ),
         ),
       ),
     );
